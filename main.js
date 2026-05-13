74f7b7e0ae7f9db5aff54d57e5b7fd741a6819db
NeKO <nekolab1108@gmail.com>
2026-05-13 15:52:57 +0800

    chore: project bootstrap

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+    },
+  })
+  win.loadFile('renderer/index.html')
+}
+
+app.whenReady().then(createWindow)
+app.on('window-all-closed', () => app.quit())
